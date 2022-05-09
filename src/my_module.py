c62dec013853eeee5c89c41a1dcc8766b4ef6bf3
MarcioJCarvalho <marciojosedecarvalho@gmail.com>
2022-05-08 22:34:05 -0300

    update: pegando o tipo de cliente

--- src/my_module.py
+++ src/my_module.py
@@ -1,3 +1,16 @@
 def get_cheapest_hotel(number):   #DO NOT change the function's name
     cheapest_hotel = "cheapest_hotel_name"
+
+    ## base de dados dos hoteis ##
+    lakewood = ["Lakewood", 110.00, 90.00, 80.00, 80.00]
+    bridgewood = ["Bridgewood", 160.00, 60.00, 110.00, 50.00]
+    ridgewood = ["Ridgewood", 220.00, 150.00, 100.00, 40.00]
+
+    data = number.split(":")
+    client_type = data[0]
+    dates = data[1]
+
+    print(client_type)
     return cheapest_hotel
+
+get_cheapest_hotel("Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)")
